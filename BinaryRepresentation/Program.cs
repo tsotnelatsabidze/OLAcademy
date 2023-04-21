b7d91bc69261ecd48dcd38e326916e8edaa576ce
tsotne latsabidze <tsotnelatsabidze@outlook.com>
2023-04-22 01:08:39 +0400

    updated

--- BinaryRepresentation/Program.cs
+++ BinaryRepresentation/Program.cs
@@ -4,11 +4,25 @@ class Program
 {
     static void Main()
     {
+        /*
         Console.Write("Enter a decimal number: ");
         int decimalNumber = int.Parse(Console.ReadLine());
 
         string binaryNumber = Convert.ToString(decimalNumber, 2);
 
         Console.WriteLine($"Binary representation of {decimalNumber}: {binaryNumber}");
+
+        */
+
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        string binary = "";
+        while (num > 0)
+        {
+            binary = (num % 2) + binary;
+            num /= 2;
+        }
+        Console.WriteLine("The binary representation is {0}", binary);
     }
+
 }
